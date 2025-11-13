c058b8266a6886c0d30fa6ca61afea716af653b8
PedroG659 <150958889+PedroG659@users.noreply.github.com>
2025-11-13 20:28:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -130,11 +130,6 @@ app.get('/metrics', async (req, res) => {
     }
 });
 
-
-/* =============================
- *  START DO SERVIDOR
- * ============================= */
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
