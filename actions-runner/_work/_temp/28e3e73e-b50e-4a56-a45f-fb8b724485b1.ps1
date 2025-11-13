47e97c8a477e583663ede40c5a96f5a9319f61af
PedroG659 <150958889+PedroG659@users.noreply.github.com>
2025-11-13 19:39:39 -0300

    comm

--- actions-runner/_work/_temp/28e3e73e-b50e-4a56-a45f-fb8b724485b1.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = 'stop'
-echo "IMAGE=seuusuario/monitor" >> $GITHUB_OUTPUT
-echo "TAG_SHA=${GITHUB_SHA::7}" >> $GITHUB_OUTPUT
-echo "NAMESPACE=default" >> $GITHUB_OUTPUT
-
-if ((Test-Path -LiteralPath variable:\LASTEXITCODE)) { exit $LASTEXITCODE }
